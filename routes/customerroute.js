e527c42e0c336ff2c05eddb8cca8f509da7604ab
R Marimuthu <marimuthurvm2020@gmail.com>
2024-03-26 23:14:22 -0700

    web api code added

---
+++ routes/customerroute.js
@@ -0,0 +1,33 @@
+var express=require('express')
+var router=express.Router();
+const customerState=require('../services/customerservice')
+const {FetchCustomers} = require("../services/customerservice");
+router.post('/customers', function(req, res, next) {
+    console.log("Testing...")
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    console.log(req.body);
+    //console.log(request);
+    //  customerState.Add(req.body);
+
+    res.send({
+        data : req,body,
+        message: "Object received successfully",
+    });
+});
+router.get('/customers', function(req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    //console.log(req.body);
+    //console.log(request);
+    // refAddState.Add(request.query.ID,request.query.Name);
+    return (FetchCustomers(req,res));
+});
+
+
+module.exports = router;
---
+++ services/customerservice.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+var CustomerModel= require('../schemas/customerschema').CustomerModel;
+
+mongoose.connect('mongodb://127.0.0.1:27017/chitdb?directConnection=true');
+
+
+//mongoose.connect(config.url, config.mongodb, config.mongoport);
+module.exports.Add=function(obj)
+{
+    var db = mongoose.connection;
+    db.once('open', function() {
+    });
+
+    var obj = new CustomerModel(
+        {
+            customerId:0,
+            firstName:obj.firstName,
+            lastName:obj.lastName,
+            middleName:"",
+            email:obj.email,
+            password:obj.password,
+            phone:obj.mobileNo
+
+        });
+    obj.save();
+
+}
+
+
+module.exports.FetchCustomers=function(req,res){
+    CustomerModel.find().then(data=>{
+        res.send(data);
+    }).catch(err=>{
+        res.status(500).send({
+            message:
+                err.message || 'Some error occurred while reading customer data'
+        });
+    });
+}
+
+module.exports.FetchCustomerById=function(req,res){
+    CustomerModel.findById(req.body.cistomerId). then(data=>{
+        res.send(data);
+    }).catch(err=>{
+        res.status(500).send({
+            message:
+                err.message || 'Some error occurred while reading customer data'
+        });
+    });
+}
+
+
